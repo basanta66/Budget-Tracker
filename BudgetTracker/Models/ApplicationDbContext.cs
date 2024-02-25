1433ba4596710732f77931e479bf2d12a5911b64
Basanta66 <123263919+basanta66@users.noreply.github.com>
2024-02-25 22:53:00 +0545

    Add files via upload

---
+++ BudgetTracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BudgetTracker.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Transaction> Transaction { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
